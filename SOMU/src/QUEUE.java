52c2884a317486fca43e62157fa78325cc2ab7af
somagouda <madagoudasomagouda@gmail.com>
2026-03-10 18:52:38 +0530

    Add DSA Java solutions

---
+++ SOMU/src/QUEUE.java
@@ -0,0 +1,5 @@
+public class QUEUE {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ so/src/rec.java
@@ -0,0 +1,15 @@
+public class rec {
+    public static void main(String[] args) {
+        num(5);
+
+    }
+    static void num(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        num(n-1);
+        System.out.println(n);
+    }
+
+}
